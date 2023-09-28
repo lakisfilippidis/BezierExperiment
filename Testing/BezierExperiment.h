52586a40200968b528ff82a9b29f3971af174816
Lakis Filippidis <lakis.filippidis@olsoft.biz>
2023-09-28 23:54:26 +0500

    Full test project

---
+++ Testing/BezierExperiment.h
@@ -0,0 +1,16 @@
+//
+//  BezierExperiment.h
+//  Testing
+//
+//  Created by Lakis Filippidis on 28/09/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BezierExperiment : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Testing/BezierExperiment.m
@@ -0,0 +1,12 @@
+//
+//  BezierExperiment.m
+//  Testing
+//
+//  Created by Lakis Filippidis on 28/09/23.
+//
+
+#import "BezierExperiment.h"
+
+@implementation BezierExperiment
+
+@end
